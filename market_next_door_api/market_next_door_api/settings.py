e9c8425385c8ba68cc98edc4136cdaa869637e2e
Dylan Timmons <dylanstimmons@icloud.com>
2023-12-05 13:46:24 -0700

    more things added

--- market_next_door_api/market_next_door_api/settings.py
+++ market_next_door_api/market_next_door_api/settings.py
@@ -11,29 +11,35 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dotenv import load_dotenv
+from dotenv import load_dotenv
 import os
 import dj_database_url
+import environ
+from environ import Env
+
 
 load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
+if env('DJANGO_ENV') == 'production':
+    DEBUG = env('DEBUG')
+else:
+    DEBUG = env('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -86,9 +92,23 @@ WSGI_APPLICATION = 'market_next_door_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
-}
+environment = os.environ.get('ENVIRONMENT')
+print(f'Current environment is set to {environment}')
+
+if environment == 'production':
+  DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+  }
+
+else:
+  DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+  }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
